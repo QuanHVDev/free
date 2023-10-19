581544c3d241c60fd5567ab04831bdde3831301f
DESKTOP-7Q0HBGC\HLC_2021 <hoangvanquan2002.unitydevelop@gmail.com>
2023-10-19 16:44:02 +0700

    set  up file

--- Assets/_Game/Scripts/MapManager.cs
+++ Assets/_Game/Scripts/MapManager.cs
@@ -1,9 +1,6 @@
 using System;
-using System.Collections;
 using System.Collections.Generic;
-using Unity.PlasticSCM.Editor.WebApi;
 using UnityEngine;
-using UnityEngine.Serialization;
 
 public class MapManager : MonoBehaviour {
 	public Action OnFinishLevel;
